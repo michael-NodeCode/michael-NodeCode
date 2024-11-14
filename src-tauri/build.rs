6b812c7e8907a5ea0e1ee805d3b147b459e35edb
mstomar698 <tomarm698@gmail.com>
2024-11-15 00:10:56 +0530

    *: MACOS KUZU fix -11

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-  tauri_build::build()
+    tauri_build::build();
+
+    if cfg!(target_os = "macos") {
+        println!("cargo:rustc-link-search=/usr/local/lib"); 
+    } else if cfg!(target_os = "windows") {
+        println!("cargo:rustc-link-search=C:\\src-tauri\\target\\release\\");
+    }
+
+    println!("cargo:rustc-link-lib=kuzu");
 }
